d39b6e4f192dbc1376188374ebaa5e73c39a74ea
Shahd-Samaneh <s12220578@stu.najah.edu>
2026-06-24 15:04:53 +0300

    Create Movie Watchlist UI and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import MovieForm from './components/MovieForm/MovieForm'
 import MovieList from './components/MovieList/MovieList'
+import { MdLocalMovies } from "react-icons/md";
+
 const moviesList = [
     {
         id: 1,
@@ -17,11 +19,19 @@ const moviesList = [
         title: "Inception",
         watched: true,
     },
-   
+
 ];
 const App = () => {
     return (
         <>
+            <header className="header container">
+                <h1>
+                    <MdLocalMovies className="movieIcon" />
+                    <span>Movie Watchlist</span>
+                    <MdLocalMovies className="movieIcon" />
+                </h1>
+                <p>Keep track of movies you want to watch</p>
+            </header>
             <MovieForm />
             <MovieList movies={moviesList} />
         </>
--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
 import styles from './MovieForm.module.css';
+import { IoAdd } from "react-icons/io5";
 
 const MovieForm = () => {
-  <>
-  </>
+
+  return (
+    <>
+      <form className={`${styles.formStyle} container`}>
+        <div className={styles.inputContainer}>
+          <input
+            type="text"
+            placeholder="Enter movie title..."
+          />
+          <button type="submit">
+            <IoAdd size={24} />
+          </button>
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default MovieForm
